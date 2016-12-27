ad5eaddb54e329714780518d021b3ecaaffe70c4
Showki <g231o026@s.iwate-pu.ac.jp>
2016-12-27 21:50:38 +0900

    add WebScrapingFunction

---
+++ web_scraping/getBooksList.php
@@ -0,0 +1,36 @@
+<?php
+require 'vendor/autoload.php';
+use Goutte\Client;
+$client = new Client();
+
+// 対象のページURL
+$url = 'xxxxxxxxxxxxxxx';
+$crawler = $client->request('GET',$url);
+
+// 押して検索するボタン
+$form = $crawler->selectButton('search')->form();
+
+// 検索したいキーワード
+$searchParameters = [
+    'keyword'       => 'アメリカ',
+    'searchtarget'  => 'BK',
+];
+
+// キーワード検索結果の取得
+$crawler = $client->submit($form, $searchParameters);
+
+// 抽出情報の絞込
+$test = $crawler->filter('div.bib-list-check ol li.book-item')->each(
+    function($element) {
+        if(count($element->filter('p.item-title a'))){
+            // $rslt['title'] = $result['title']    = $element->filter('p.item-title a')->text();
+            $rslt['title']  = str_replace(
+                array("\r\n", "\r", "\n"),'',$element->filter('p')->eq(0)->text());
+            $rslt['author'] = str_replace(
+                array("\r\n", "\r", "\n"),'',$element->filter('p')->eq(1)->text());
+            $rslt['place']  = str_replace(
+                array("\r\n", "\r", "\n"),'',$element->filter('p')->eq(2)->text());
+            return $rslt;
+    }
+});
+print_r($test);
